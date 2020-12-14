cdccca1b0eb2f32f16c2e0d36be5ca26172f9f75
Radwan Noor <ernoor@gmail.com>
2020-12-14 22:33:02 +0300

    pointer_exercise

---
+++ My_workspace/host/004pointer_L/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 14, 2020
+ *      Author: radwan
+ */
+
+
+#include<stdio.h>
+int main(void)
+{
+//	long long int someAddress = 0x00007FFF8E3C3824; // not a valid pointer
+	char* address1 = (char*) 0x00007FFF8E3C3824;
+}
